cff0f6578bdec0b01b143916e48963fafbb1a06f
Hector Saul Maldonado Gomez <saul_99_27@hotmail.com>
2020-10-27 23:20:11 -0600

    Avance Juego Basta

---
+++ JuegoBasta/Juego.cs
@@ -0,0 +1,159 @@
+﻿using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace JuegoBasta
+{
+    public enum Comando { NombreEnviado }
+    public class Juego : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void ActualizarValor(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        void CambiarMensaje(string msj)
+        {
+            currentDispatcher.Invoke(new Action(() =>
+            {
+
+                Mensaje = msj;
+                ActualizarValor();
+            }));
+        }
+
+        public string Jugador1 { get; set; } = "Jugador";
+        public string Jugador2 { get; set; }
+        public string IP { get; set; } = "localhost";
+        public string Mensaje { get; set; }
+        public bool MainVisible { get; set; } = true;
+        public ICommand IniciarCommand { get; set; }
+
+        HttpListener servidor;
+        ClientWebSocket cliente;
+        Dispatcher currentDispatcher;
+        WebSocket webSocket;
+
+        public Juego()
+        {
+            currentDispatcher = Dispatcher.CurrentDispatcher;
+            IniciarCommand = new RelayCommand<bool>(IniciarPartida);
+        }
+
+
+        // SI CREA PARTIDA SE INICIA COMO SERVIDOR
+        public void CrearPartida()
+        {
+            servidor = new HttpListener();
+            servidor.Prefixes.Add("http://*:8080/basta/");
+            servidor.Start();
+            servidor.BeginGetContext(OnContext, null);
+
+            Mensaje = "Esperando adversario...";
+            ActualizarValor();
+        }
+        // SI SE UNE A UNA PARTIDA INICIA COMO CLIENTE
+        public async Task ConectarPartida()
+        {
+            cliente = new ClientWebSocket();
+            await cliente.ConnectAsync(new Uri($"ws://{IP}:8080/basta/"), CancellationToken.None);
+            webSocket = cliente;
+
+            // RecibirComando();
+        }
+        // CUANDO RECIBE UNA CONEXIÓN CON EL CLIENTE
+        private async void OnContext(IAsyncResult ar)
+        {
+            var context = servidor.EndGetContext(ar);
+
+            if (context.Request.IsWebSocketRequest)
+            {
+                var listener = await context.AcceptWebSocketAsync(null);
+                webSocket = listener.WebSocket;
+                CambiarMensaje("Conexión exitosa. Esperando información del contrincante.");
+                EnviarComando(new DatoEnviado { Comando = Comando.NombreEnviado, Dato = Jugador1 });
+
+                // RecibirComando();
+
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+                context.Response.Close();
+                servidor.BeginGetContext(OnContext, null);
+            }
+        }
+
+        LobbyWindow lobby;
+        // CUANDO YA SE INICIÓ EL SERVIDOR Y EL CLIENTE
+        private async void IniciarPartida(bool tipoP)
+        {
+            try
+            {
+                MainVisible = false;
+                lobby = new LobbyWindow();
+                lobby.Closing += Lobby_Closing;
+                lobby.DataContext = this;
+                lobby.Show();
+                ActualizarValor();
+
+                if (tipoP == true)
+                {
+                    CrearPartida();
+                }
+                else
+                {
+                    Jugador2 = Jugador1;
+                    Jugador1 = null;
+                    Mensaje = "Intentando conectar con el contrincante en " + IP;
+                    ActualizarValor("Mensaje");
+                    await ConectarPartida();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Mensaje = ex.Message;
+                ActualizarValor();
+            }
+
+        }
+
+        private void Lobby_Closing(object sender, CancelEventArgs e)
+        {
+            MainVisible = true;
+            ActualizarValor("MainVisible");
+            if (servidor != null)
+            {
+                servidor.Stop();
+                servidor = null;
+            }
+        }
+
+        public async void EnviarComando(DatoEnviado datos)
+        {
+            byte[] buffer;
+            var json = JsonConvert.SerializeObject(datos);
+            buffer = Encoding.UTF8.GetBytes(json);
+            await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+    }
+    public class DatoEnviado
+    {
+        public Comando Comando { get; set; }
+        public object Dato { get; set; }
+    }
+}
